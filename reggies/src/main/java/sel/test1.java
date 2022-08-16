7095abdf1b99fc424d7d7769925ab08af89f6e15
Pushpender <pushpendersp21@gmail.com>
2022-08-16 10:23:09 +0530

    Back to Normal

--- reggies/src/main/java/sel/test1.java
+++ reggies/src/main/java/sel/test1.java
@@ -17,9 +17,7 @@ public class test1 {
 		//driver.get("https://www.google.com");
 		genericLib.initialize();
 		genericLib.navigateTo("https://www.google.co.in/");
-		System.out.print("Pushpa");
 		genericLib.quit();
-		System.out.print("Pushpa2");
 		
 	}
 
