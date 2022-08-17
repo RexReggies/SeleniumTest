a2bf0b5c7fae515aea378713be466af4e0d9c79a
reggies_anthony <reggies.anthony@oracle.com>
2022-08-18 03:16:49 +0530

    Adding css , getWebElement , set methods.

--- reggies/src/main/java/sel/test1.java
+++ reggies/src/main/java/sel/test1.java
@@ -10,14 +10,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class test1 {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		//WebDriverManager.chromedriver().setup();
 		//WebDriver driver = new ChromeDriver();
 		//driver.get("https://www.google.com");
 		genericLib.initialize();
 		genericLib.navigateTo("https://www.google.co.in");
-		genericLib.Click("(//input[@aria-label='Google Search'])[2]");
+		genericLib.set("css=input[title='Search']","Google");
+		//genericLib.Click("css=span[class*=' MZy1Rb']>svg");
+		genericLib.Click("(//input[@value='Google Search'])[2]");
+		genericLib.WaitForElement("xpath=(//*[text()='Google'])[1]",5);
+		genericLib.Click("xpath=(//*[text()='Google'][1])");
+		genericLib.hardAssert("css=img[alt='Google']",true);
+		genericLib.hardAssert("xpath=//img[@alt='Google']",true);
+		//genericLib.hardAssert("xpath=//*[text()='Negative Check']",false);
 		System.out.print("Basic Frame Work Check Completed");
 		genericLib.quit();
 	}
