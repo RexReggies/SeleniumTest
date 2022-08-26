171092406fb957a649f918ac01f0881aa0f5942e
reggies_anthony <reggies.anthony@oracle.com>
2022-08-27 02:04:50 +0530

    test

--- reggies/src/main/java/stepdefination/test.java
+++ reggies/src/main/java/stepdefination/test.java
@@ -7,7 +7,7 @@ import io.cucumber.java.en.When;
 public class test {
 	@Given("I Launch Google ChromeBrowser")
 	public void i_launch_google_chrome_browser() {
-	    // Write code here that turns the phrase above into concrete actions
+	    // Write code here that turns the phrase above into concrete actions//
 	    throw new io.cucumber.java.PendingException();
 	}
 
